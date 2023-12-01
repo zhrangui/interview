db96912dc0608383e02131fe1463da2da96d010e
Rui Zhang <zhrangui@hotmail.com>
2023-12-01 08:44:06 -0500

    88-Merge-Sorted-Array

--- src/main/kotlin/88-Merge-Sorted-Array.kt
+++ src/main/kotlin/88-Merge-Sorted-Array.kt
@@ -3,16 +3,16 @@ import org.junit.jupiter.api.Test
 
 class Solution() {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): UInt {
-        return (nums1.size + m + nums2.size + n).toUInt();
+        return (nums1.size + m + nums2.size + n).toUInt()
     }
 
     @Test
     fun testMerge() {
-        val nums1 = intArrayOf(1,2,3,0,0,0)
+        val nums1 = intArrayOf(1, 2, 3, 0, 0, 0)
         val m = 3
-        val nums2 = intArrayOf(2,5,6)
+        val nums2 = intArrayOf(2, 5, 6)
         val n = 3
-        val expected = intArrayOf(1,2,2,3,5,6)
+        val expected = intArrayOf(1, 2, 2, 3, 5, 6)
         assertEquals(expected, merge(nums1, m, nums2, n))
     }
 }
