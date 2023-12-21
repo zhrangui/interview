8b39b8634ff00859938f6d94e185e881c88d0be6
Rui Zhang <zhrangui@outlook.com>
2023-12-21 11:40:16 -0500

    kotlin 1.9.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -34,7 +34,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
