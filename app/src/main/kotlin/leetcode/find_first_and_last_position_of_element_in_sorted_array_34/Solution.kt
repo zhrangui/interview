215c898cc58557f06cd79e8178f607c4bbe3827d
Rui Zhang <zhrangui@outlook.com>
2023-12-27 08:27:26 -0500

    34. Find First and Last Position of Element in Sorted Array

---
+++ app/src/main/kotlin/leetcode/find_first_and_last_position_of_element_in_sorted_array_34/Solution.kt
@@ -0,0 +1,8 @@
+package leetcode.find_first_and_last_position_of_element_in_sorted_array_34
+
+class Solution {
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        val pos = intArrayOf(0,0)
+        return pos
+    }
+}
---
+++ app/src/test/kotlin/leetcode/find_first_and_last_position_of_element_in_sorted_array_34/SolutionTest.kt
@@ -0,0 +1,12 @@
+package leetcode.find_first_and_last_position_of_element_in_sorted_array_34
+
+import kotlin.test.Test
+class SolutionTest {
+    @Test fun searchRange() {
+        val classUnderTest = Solution()
+        val nums =  intArrayOf(5,7,7,8,8,10)
+        val target = 8
+        val pos = classUnderTest.searchRange(nums, target)
+        assert(pos.contentEquals(intArrayOf(-1,-1)))
+    }
+}
